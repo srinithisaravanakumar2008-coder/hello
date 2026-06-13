86e2dc8f865f754b76c334f7ed9838f587e86de1
Srinithi Saravanakumar <srinithisaravanakumar2008@gmail.com>
2026-06-13 14:27:53 +0530

    enum

--- src/Day8.java
+++ src/Day8.java
@@ -1,4 +1,4 @@
-public class Day8{
+/*public class Day8{
     record CourseRecord(String coursename,String dept){
 
     }
@@ -7,4 +7,23 @@ public class Day8{
        System.out.println(courseRecord.coursename());
        System.out.println(courseRecord.dept());
    }
-}
+}*/
+    enum LANGUAGE{
+        EN("English"),
+        GR("German"),
+        JP("Japanese"),
+        TA("Tamil");
+
+        String description;
+
+        LANGUAGE(String description){
+            this.description = description;
+        }
+    }
+
+    static void main() {
+        System.out.println(LANGUAGE.EN.description);
+        System.out.println(LANGUAGE.GR.description);
+        System.out.println(LANGUAGE.JP.description);
+        System.out.println(LANGUAGE.TA.description);
+    }
