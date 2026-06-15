5cd03da14543e2beb3f2a7dc10dd8cb55003a72d
Srinithi Saravanakumar <srinithisaravanakumar2008@gmail.com>
2026-06-15 09:43:47 +0530

    Login

--- src/Day8.java
+++ src/Day8.java
@@ -8,7 +8,7 @@
        System.out.println(courseRecord.dept());
    }
 }*/
-    enum LANGUAGE{
+   /* enum LANGUAGE{
         EN("English"),
         GR("German"),
         JP("Japanese"),
@@ -26,4 +26,42 @@
         System.out.println(LANGUAGE.GR.description);
         System.out.println(LANGUAGE.JP.description);
         System.out.println(LANGUAGE.TA.description);
+    }*/
+public class LoginExample{
+
+// login
+// Success, Failure, loading
+sealed interface LoginResult permits LoginSuccess, LoginFailure, LoginLoading {
+
+}
+record LoginSuccess(String time) implements LoginResult {
+
+}
+record LoginFailure(String reason)  implements LoginResult {
+
+}
+
+record LoginLoading(String expectedTime)  implements LoginResult {
+
+}
+
+record Random(String reason){
+
+}
+static void handleLogin(LoginResult result) {
+    switch (result) {
+        case LoginSuccess success -> System.out.println("Login Success at : " + success.time());
+        case LoginFailure failure -> System.out.println("Login Failure because of " + failure.reason());
+        case LoginLoading loading -> System.out.println("Login Loading, " + loading.expectedTime());
     }
+}
+static void main() {
+    LoginResult  LoginSuccess = new LoginSuccess("2026-06-15:05:07:25");
+    LoginResult LoginFailure = new LoginFailure("Network issue");
+    LoginResult LoginLoading= new LoginLoading("Please wait for few minutes");
+
+    handleLogin(LoginSuccess);
+    handleLogin(LoginFailure);
+    handleLogin(LoginLoading);
+}
+}
